7b7df2ed28e1ed54142773aab163eb3082d73595
NoxPointe <blackromance@gmail.com>
2013-06-12 15:06:47 -0500

    Made objects[] a trait of the Map class
    This allows object data and map data to be stored together in a Map
    object.  This will be useful when saving and loading specific map data.

--- roguelike/firstrl.py
+++ roguelike/firstrl.py
@@ -199,9 +199,9 @@ class Object:
 		
 	def send_to_back(self):
 		#make this object be drawn first, so all others appear above it if they're in the same tile
-		global objects
-		objects.remove(self)
-		objects.insert(0, self)
+		global map
+		map.objects.remove(self)
+		map.objects.insert(0, self)
 
 class Fighter:
 	#combat related properties and methods (monster, player, NPC)
@@ -323,7 +323,7 @@ class Item:
 			message('Your inventory is full, cannot pick up ' + self.owner.name + '.', libtcod.red)
 		else:
 			inventory.append(self.owner)
-			objects.remove(self.owner)
+			map.objects.remove(self.owner)
 			message('You picked up a ' + self.owner.name + '!', libtcod.green)
 		
 		#special case: automatically equip, if the corresponding equipment slot is unused
@@ -345,7 +345,7 @@ class Item:
 				
 	def drop(self):
 		#add to the map and remove from the player's inventory. also, place it at the player's coordinates
-		objects.append(self.owner)
+		map.objects.append(self.owner)
 		inventory.remove(self.owner)
 		self.owner.x = player.x
 		self.owner.y = player.y
@@ -480,7 +480,7 @@ def play_game():
 		check_level_up()
 	
 		#erase all objects at their old locations, before they move
-		for object in objects:
+		for object in map.objects:
 			object.clear()
 	
 		#handle keys and exit game if needed
@@ -499,28 +499,26 @@ def save_game():
 	#open a new empty shelve (possibly overwriting an old one) to write the game data
 	file = shelve.open('savegame', 'n')
 	file['map'] = map
-	file['objects'] = objects
-	file['player_index'] = objects.index(player) #index of player in objects list
+	file['player_index'] = map.objects.index(player) #index of player in objects list
 	file['inventory'] = inventory
 	file['game_msgs'] = game_msgs
 	file['game_state'] = game_state
-	file['stairs_index'] = objects.index(stairs)
+	file['stairs_index'] = map.objects.index(stairs)
 	file['depth'] = depth
 	file.close()
 
 def load_game():
 	#open the previously saved shelve and load the game data
-	global map, objects, player, inventory, game_msgs, game_state, stairs
+	global map, player, inventory, game_msgs, game_state, stairs
 	global depth
 	
 	file = shelve.open('savegame', 'r')
 	map = file['map']
-	objects = file['objects']
-	player = objects[file['player_index']] #get index of player in objects list and access it
+	player = map.objects[file['player_index']] #get index of player in objects list and access it
 	inventory = file['inventory']
 	game_msgs = file['game_msgs']
 	game_state = file['game_state']
-	stairs = objects[file['stairs_index']]
+	stairs = map.objects[file['stairs_index']]
 	depth = file['depth']
 	file.close()
 	
@@ -568,7 +566,7 @@ def player_move_or_attack(dx, dy):
 	
 	#try to find an attackable object there
 	target = None
-	for object in objects:
+	for object in map.objects:
 		if object.fighter and object.x == x and object.y == y:
 			target = object
 			break
@@ -617,7 +615,7 @@ def handle_keys():
 			
 			if key_char == 'g':
 				#pick up an item
-				for object in objects: #look for an item in the player's tile
+				for object in map.objects: #look for an item in the player's tile
 					if object.x == player.x and object.y == player.y and object.item:
 						object.item.pick_up()
 						break
@@ -654,7 +652,7 @@ def get_names_under_mouse():
 	(x, y) = (camera_x + x, camera_y + y) #from screen to map coordinates
 	
 	#create a list with the names of all objects at the mouse's coordinates and in FOV
-	names = [obj.name for obj in objects
+	names = [obj.name for obj in map.objects
 		if obj.x == x and obj.y == y and libtcod.map_is_in_fov(fov_map, obj.x, obj.y)]
 		
 	names = ', '.join(names) #join the names, seperated by commas
@@ -692,18 +690,18 @@ def is_blocked(x, y):
 		return True
 		
 	#now check for any blocking objects
-	for object in objects:
+	for object in map.objects:
 		if object.blocks and object.x == x and object.y == y:
 			return True
 			
 	return False
 	
 def make_map():
-	global map, objects, stairs, depth
+	global map, stairs, depth
 	
 	#the list of objects with just the player
 	map = Map()
-	objects = [player]
+	map.objects = [player]
 	ticker.recalculate()
 	
 	if depth == 0:
@@ -788,7 +786,7 @@ def make_map():
 			
 	#create stairs at the center of the last room
 	stairs = Object(new_x, new_y, '>', 'stairs', libtcod.white, always_visible = True)
-	objects.append(stairs)
+	map.objects.append(stairs)
 	stairs.send_to_back() #so it's drawn below monsters
 
 def next_level():
@@ -885,7 +883,7 @@ def render_all():
 					map.tiles[map_x][map_y].explored = True
 					
 	#draw all objects in the list
-	for object in objects:
+	for object in map.objects:
 		if object != player:
 			object.draw()
 	player.draw()
@@ -966,7 +964,7 @@ def place_objects(room):
 				fighter_component = Fighter(hp=5, defense=1, power=100, speed=5, xp=2, death_function = monster_death)
 				monster = Object(x, y, '@', 'dungeon bunny', libtcod.light_yellow, blocks=True, fighter=fighter_component, ai=ai_component)
 			
-			objects.append(monster)
+			map.objects.append(monster)
 			
 	#choose random number of items
 	num_items = libtcod.random_get_int(0, 0, max_items)
@@ -1004,7 +1002,7 @@ def place_objects(room):
 				equipment_component = Equipment(slot='left hand', defense_bonus=1)
 				item = Object(x, y, '[', 'shield', libtcod.darker_orange, equipment=equipment_component)
 			
-			objects.append(item)
+			map.objects.append(item)
 			item.send_to_back() #items appear below other objects
 
 def menu(header, options, width):
@@ -1142,7 +1140,7 @@ def cast_fireball():
 	if x is None: return 'cancelled'
 	message('The fireball explodes, burning everything within ' + str(FIREBALL_RADIUS) + ' tiles!', libtcod.orange)
 	
-	for obj in objects: #damage every fighter in range, including the player
+	for obj in map.objects: #damage every fighter in range, including the player
 		if obj.distance(x,y) <= FIREBALL_RADIUS and obj.fighter:
 			message('The ' + obj.name + ' gets burned for ' + str(FIREBALL_DAMAGE) + ' hit points.', libtcod.orange)
 			obj.fighter.take_damage(FIREBALL_DAMAGE)
@@ -1152,7 +1150,7 @@ def closest_monster(max_range):
 	closest_enemy = None
 	closest_dist = max_range + 1
 	
-	for object in objects:
+	for object in map.objects:
 		if object.fighter and not object == player and libtcod.map_is_in_fov(fov_map, object.x, object.y):
 			#calculate distance between this object and the player
 			dist = player.distance_to(object)
@@ -1187,7 +1185,7 @@ def target_monster(max_range = None):
 			return None
 		
 		#return the first clicked monster, otherwise continue looping
-		for obj in objects:
+		for obj in map.objects:
 			if obj.x == x and obj.y == y and obj.fighter and obj != player:
 				return obj
 
